b61e2bce9dc55582d337e2241c4514c8d3f01a4b
Alexander P Nohe <anohe@esri.com>
2014-05-08 15:03:02 -0700

    First second commit
    Here is what is done so far.  Legend is MIA.

---
+++ JSAPI/home.js
@@ -0,0 +1,37 @@
+var map;
+var mapOptions = ["osm", "national-geographic", "gray", "satellite", "hybrid", "oceans", "streets", "topo"];
+var legOpt = {
+	map:map,
+	autoUpdate: true,
+	respectCurrentMapScale: true
+};
+require(["esri/map", "esri/dijit/Legend", "esri/layers/FeatureLayer", "dojo/domReady!"],
+function (Map, Legend, FeatureLayer) {
+        map = new Map("mapDiv", {
+            center: [-75.8, 38.45],
+            zoom: 9,
+            basemap: "gray",
+        });
+
+var layer = new FeatureLayer("http://services.arcgis.com/Wl7Y1m92PbjtJs5n/arcgis/rest/services/autoBody/FeatureServer/0",
+	{visible: true
+});
+
+var legend = new Legend({
+	map:map
+}, "legDiv");
+legend.startup;
+
+
+
+map.addLayer(layer);
+
+function mapSelect(){
+	num = Math.floor(Math.random() * mapOptions.length);
+	console.log(num)
+	basemap = mapOptions[num];
+	console.log(basemap)
+	return basemap;
+};
+
+});
